f7cf71197a47829a165412a7fdecd91d2ec7ce78
selim <selimb30@outlook.fr>
2026-03-22 14:42:09 +0100

    refonte total

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-function submitForm() {
-  var objet = document.getElementById("mail_objet").value;
-  var message = document.getElementById("mail_message").value;
-  var destinataire = "selimb30@outlook.fr";
-  var mailtoLink = "mailto:" + destinataire + "?subject=" + objet + "&body=" + message;
-  window.location.href = mailtoLink;
-}
+const reveals = document.querySelectorAll(".reveal");
 
-  function bannerSwitcher() {
-    next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
-    if (next.length) next.prop('checked', true);
-    else $('.sec-1-input').first().prop('checked', true);
-  }
+window.addEventListener("scroll", () => {
+    reveals.forEach((el) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
 
-  var bannerTimer = setInterval(bannerSwitcher, 5000);
-
-  $('nav .controls label').click(function() {
-    clearInterval(bannerTimer);
-    bannerTimer = setInterval(bannerSwitcher, 5000)
-  });
-
-var lang = {
-    "html": "avancé",
-    "css": "avancé",
-    "javascript": "intermédiaire",
-    "php": "avancé",
-    "wordpress": "intermédiaire",
-    "bootstrap": "intermédiaire",
-    "laravel": "débutant"
-};
-var multiply = 4;
-
-$.each( lang, function( language, pourcent) {
-
-        var delay = 700;
-
-        setTimeout(function() {
-            $('#'+language+'-pourcent').html(pourcent);
-        },delay*multiply);
-
-        multiply++;
-
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("active");
+        }
     });
+});
